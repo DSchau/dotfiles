18ab31556896d9354f4ca15f9234c80b83ef9dab
Dustin Schau <dustinschau@gmail.com>
2023-02-25 10:19:33 -0800

    chore: add settings

--- init/init.sh
+++ init/init.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 
-echo $HOME
-
-# create code directories
+# create project directories
 CODE=${HOME}/Projects
 
 mkdir -p $CODE
 mkdir -p ${CODE}/Personal
 mkdir -p ${CODE}/Work
 
+###############################################################################
+# VSCode                                                                      #
+###############################################################################
+
+# move VSCode settings
+mkdir -p ${HOME}/Library/Application\ Support/Code/User/
+cp -r VSCode/. ${HOME}/Library/Application\ Support/Code/User/
+
+# make sure to install extensions too
+## important ones
+# Dracula
+# Markdown All in One
+# Prettier
+# Sublime Text Keymaps
+
+###############################################################################
+# ZSH                                                                         #
+###############################################################################
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
