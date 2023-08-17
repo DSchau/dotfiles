50d567822dd56a0a458c593018bca25fdd60ed66
Dustin Schau <dustinschau@gmail.com>
2023-08-17 09:51:55 -0700

    chore: updates to drop script

--- scripts/drop.sh
+++ scripts/drop.sh
@@ -6,24 +6,36 @@ INPUT_FOLDER="$HOME/Movies/Drop/"
 # Output folder where converted files will be stored
 OUTPUT_FOLDER="$HOME/Movies/Home"
 
+LOG_LEVEL="info"
+
 # Ensure output folder exists
 mkdir -p "$OUTPUT_FOLDER"
 
+total_files=$(find "$INPUT_FOLDER" -type f -iname "*.mov" | wc -l | awk '{print $1}')
+
+echo "Converting total of $total_files files"
+
 # Iterate over all files in the input folder
-find "$INPUT_FOLDER" -type f | while read -r file; do
+find "$INPUT_FOLDER" -type f -iname "*.mov" | while read -r file; do
     # Extract the filename without the path or extension for naming the output file
     filename=$(basename -- "$file")
     filename_no_ext="${filename%.*}"
+
+    ((current_file++))
+
+    output_file="$OUTPUT_FOLDER/$filename_no_ext.mp4"
+    progress="($current_file / $total_files)"
     
     # Convert the file to mp4 format using ffmpeg
-    ffmpeg -i "$file" "$OUTPUT_FOLDER/$filename_no_ext.mp4" -hide_banner -loglevel error
+    ffmpeg -y -nostdin -i "$file" "$output_file" -hide_banner -loglevel $LOG_LEVEL
     
     # Check if the conversion was successful
     if [ $? -eq 0 ]; then
+        echo "✅ $progress Converted $file to $output_file"
         # If the conversion was successful, delete the original file
         rm "$file"
     else
         # Optionally, print an error message if the conversion failed
-        echo "Failed to convert $file"
+        echo "❌ $progress Failed to convert $file"
     fi
 done
